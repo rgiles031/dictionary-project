df8ba7e96328a01974d581581f7c7f774eae91c4
rgiles031 <rgiles031@gmail.com>
2023-07-29 18:03:22 +0100

    Removed phonetics component
    removed phonetics component as only contained 3 lines, moved to Results component

--- src/Phonetic.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Phonetic(props) {
-  return props.phonetic.phonetic;
-}
--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Meaning from "./Meaning";
-import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="results">
         <h3>{props.results.word}</h3>
-        <Phonetic phonetic={props.results} />
+        <p>{props.results.phonetic}</p>
         {props.results.meanings.map(function(meaning, index) {
           return (
             <div key={index}>
