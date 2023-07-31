9e03af88c37696ae4e3941d93bfaf680ca7cfbd4
rgiles031 <rgiles031@gmail.com>
2023-07-31 20:17:34 +0100

    Added Pexels API for photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiKey = "e3f54t4fb8o2b093a97023c59f08b2d1";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey = `O9sLf5tuUnUJg3JsiLXibGvti10d4IvKsLHQ6MvKGN1s9OpqbJ6g6fBz`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -46,6 +60,7 @@ export default function Dictionary(props) {
           <div className="hint">Suggested words: sunset, dog, beach...</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Photos(props) {
+  console.log(props);
+  if (props.photos) {
+    return (
+      <section>
+        <div className="row">
+          {props.photos.map(function(photo, index) {
+            return (
+              <div className="col-4">
+                <img
+                  key={index}
+                  src={photo.src.landscape}
+                  className="img-fluid photo"
+                />
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+
+  return <div></div>;
+}
